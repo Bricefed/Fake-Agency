d8e558f495b57747bb8ecc8e1da08b932c32f68a
Fedou Brice <napofedoupro@gmail.com>
2022-09-27 15:05:26 +0200

    Create component Buttons (navigation)

---
+++ src/components/Home/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="home-main">
+      <div className="main-content">
+        <h1>FAKE AGENCY</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,19 @@ import React from "react";
 import Navigation from "../components/Navigation/Navigation";
 import Mouse from "../components/Mouse";
 import SocialNetwork from "../components/SocialNetwork/SocialNetwork";
+import Header from "../components/Home/Header";
+import Buttons from "../components/Buttons";
 
 const Home = () => {
   return (
     <div>
       <Mouse />
-      <Navigation />
-      <SocialNetwork />
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <Header />
+        <Buttons right={"/project-1"} />
+      </div>
     </div>
   );
 };
